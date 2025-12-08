b1957dea28c57dce174c09f30e1c8fef21d54cde
Frann-debug <fmonteroa06@informatica.iesvalledeljerteplasencia.es>
2025-12-08 10:34:06 -0100

    Ejercicio 7 - Tema 6

---
+++ TEMA 6/Ejercicio7/src/ejercicio7/ControlRemoto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ejercicio7;
+
+/**
+ *
+ * @author fmont
+ */
+public interface ControlRemoto {
+    // MÉTODOS
+    public void apagar();
+    public void encender();
+    public void bajarVolumen();
+    public void subirVolumen();
+    public void cambiarCanal(float canal);
+    
+}
