a84e0da1cc23e16335f85415cb837f4a8127dc83
Frann-debug <fmonteroa06@informatica.iesvalledeljerteplasencia.es>
2026-01-30 19:22:47 -0100

    Tema 8 - Ejercicio 7

--- TEMA 8/Ejercicio7/src/ejercicio7/Ejercicio7.java
+++ TEMA 8/Ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -9,11 +9,6 @@ import java.util.Scanner;
  * @author alumno
  */
 public class Ejercicio7 {
-    public static Dia[] array() {
-        Dia[] dias = new Dia[30];
-        return dias;
-    }
-    
     public static int menu() {
         Scanner entrada = new Scanner(System.in);
         int opcion;
@@ -25,27 +20,77 @@ public class Ejercicio7 {
         return opcion;
     }
     
-    public static void rellenarTemperaturas(Dia[] dias) {
+    public static Dia[] rellenarTemperaturas() {
+        Dia[] dias = new Dia[30];
         int i;
         String[] diasSemana = {"Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"}; 
         for(i = 0; i < dias.length; i++) {
             int diaSemanaAleatorio = (int)(Math.random() * 7);
             String nombreDia = diasSemana[diaSemanaAleatorio];
-            int temperatura = (int)(Math.random() * (40 - 0 + 1) + 0);
+            int temperatura = (int)(Math.random() * (45 - 0 + 1) + 0);
             dias[i] = new Dia(nombreDia,temperatura);
+        }
+        return dias;
+    }
+    
+    public static void mostrarTemperaturas(Dia[] dias) {
+        int i;
+        for(i = 0; i < dias.length; i++) {
+            System.out.println(""+dias[i].getNombreDia()+" día "+(i+1)+": "+dias[i].getTemperatura()+" grados.");
         }
     }
     
+    public static void mediaTemperatura(Dia[] dias) {
+        int i;
+        int suma = 0;
+        int media;
+        for(i = 0; i < dias.length; i++) {
+            suma = suma + dias[i].getTemperatura();
+        }
+        media = suma / dias.length;
+        System.out.println("La temperatura media del mes es de: "+media+"");
+    }
+    
+    public static void mostrarDiaoDiasMasCalurosos(Dia[] dias) {
+        int i;
+        int mayor  = dias[0].getTemperatura();
+        for(i = 0; i < dias.length; i++) {
+            if(dias[i].getTemperatura() > mayor) {
+                mayor = dias[i].getTemperatura();
+            }
+        }
+        for(i = 0; i < dias.length; i ++) {
+            if(dias[i].getTemperatura() == mayor) {
+                System.out.println("El "+dias[i].getNombreDia()+" día "+(i+1)+" con "+mayor+"");
+            }
+        }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Dia[] dias = new Dia[30];
         int opcion;
         do {
             opcion = menu();
             switch(opcion) {
                 case 1:
-                    
+                    dias = rellenarTemperaturas();
+                    break;
+                case 2:
+                    mostrarTemperaturas(dias);
+                    break;
+                case 3:
+                    mediaTemperatura(dias);
+                    break;
+                case 4:
+                    mostrarDiaoDiasMasCalurosos(dias);
+                    break;
+                case 5:
+                    System.out.println("Te has aalido del programa.");
+                    break;
+                default:
+                    System.out.println("ERROR. Introduce un número correcto.");
             }
             
         } while(opcion != 5);
