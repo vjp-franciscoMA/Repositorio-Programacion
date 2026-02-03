f9e47ea9f0d9b338136121dd33ca3221c5ecc82b
vjp-franciscoMA <fmonteroa06@informatica.iesvalledeljerteplasencia.es>
2026-02-03 18:00:23 +0100

    Tema 8 - Ejercicio13y14 modificado

--- TEMA 8/Ejercicio13y14/src/ejercicio13y14/Ejercicio13y14.java
+++ TEMA 8/Ejercicio13y14/src/ejercicio13y14/Ejercicio13y14.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Ejercicio13y14 {
     public static int menu() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("1. Rellenar alumno.");
+        System.out.println("1. Rellenar alumnos.");
         System.out.println("2. Mostrar alumnos.");
         System.out.println("3. Mostrar alumnos con nota media por encima de una nota dada.");
         System.out.println("4. Mostrar cuántos alumnos hay con la nota media suspensa.");
@@ -33,35 +33,88 @@ public class Ejercicio13y14 {
             System.out.println("Introduce la nota media del alumno "+(i+1)+"");
             double notaMedia = entrada.nextDouble();
             entrada.nextLine();
-            System.out.println("Introduce la posición del alumno que quieres crear:");
+            System.out.println("Introduce la posición del alumno que quieres crear (0-4):");
             int posicion = entrada.nextInt();
             entrada.nextLine();
+            while(alumnos[posicion] != null) {
+                System.out.println("La posición introducida está ocupada. Elige otra:");
+                posicion = entrada.nextInt();
+                entrada.nextLine();
+            }
             alumnos[posicion] = new Alumno(nombre, edad, notaMedia);
         }
         return alumnos;
+    }
+    public static void mostrarAlumnos(Alumno[] alumnos) {
+        int i;
+        for(i = 0; i < alumnos.length; i++) {
+            System.out.println(alumnos[i]);
+        }
+    }
+    public static void mostrarAlumnosMedia(Alumno[] alumnos) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce una nota para que te salgan los alumnos que tengan una nota media superior a la nota introducida:");
+        double nota = entrada.nextDouble();
+        entrada.nextLine();
+        int i;
+        for (i = 0; i < alumnos.length; i++) {
+            if(alumnos[i].getNotaMedia() > nota) {
+                System.out.println("La media del alumno "+alumnos[i].getNombre()+" es "+alumnos[i].getNotaMedia()+"");
+            }
+        }
+    }
+    public static void mostrarAlumnosMediaSuspensa(Alumno[] alumnos) {
+        int i;
+        for (i = 0; i < alumnos.length; i++) {
+            if(alumnos[i].getNotaMedia() < 5) {
+                System.out.println("La media del alumno "+alumnos[i].getNombre()+" es "+alumnos[i].getNotaMedia()+"");
+            }
+        }
+    }
+    public static void buscarAlumnos(Alumno[] alumnos) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce el nombre de un alumno para ver si está matriculado:");
+        String nombreAlumno = entrada.nextLine();
+        boolean encontrado = false;
+        int i = 0;
+        while (i < alumnos.length && !encontrado) {
+            if (alumnos[i].getNombre().equals(nombreAlumno)) {
+                encontrado = true;
+            }
+            else {
+                i++;
+            }
+        }
+        if (encontrado == true) {
+            System.out.println("El alumno "+nombreAlumno+" está matriculado.");
+        }
+        else {
+            System.out.println("El alumno "+nombreAlumno+" no está matriculado.");
+        }
     }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         int opcion;
+        Alumno[] alumnos = new Alumno[5];
         do {
             opcion = menu();
             switch(opcion) {
                 case 1:
-                    
+                    alumnos = rellenarAlumnos();
                     break;
                 case 2:
-                    
+                    mostrarAlumnos(alumnos);
                     break;
                 case 3:
-                    
+                    mostrarAlumnosMedia(alumnos);
                     break;
                 case 4:
-                    
+                    mostrarAlumnosMediaSuspensa(alumnos);
                     break;
                 case 5:
-                    
+                    buscarAlumnos(alumnos);
                     break;
                 case 6:
                     System.out.println("Te has salido del programa.");
