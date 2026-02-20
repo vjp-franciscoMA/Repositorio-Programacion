f314f57e52d8be8488d9a197f421ff198f90290f
vjp-franciscoMA <fmonteroa06@informatica.iesvalledeljerteplasencia.es>
2026-02-20 19:57:23 +0100

    Tema 9 - Ejercicio 9

--- TEMA 9/Ejercicio9/src/ejercicio9/Ejercicio9.java
+++ TEMA 9/Ejercicio9/src/ejercicio9/Ejercicio9.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio9;
-
+import java.util.Scanner;
 /**
  *
  * @author alumno
@@ -14,7 +14,45 @@ public class Ejercicio9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce un usuario:");
+        String usuario = entrada.nextLine();
+        
+        String contraseña;
+        boolean valida;
+        
+        do {
+            System.out.println("Introduce una contraseña:");
+            contraseña = entrada.nextLine();
+            
+            valida = true;
+            
+            if (contraseña.length() < 6) {
+                System.out.println("La contraseña no puede tener menos de seis caracteres.");
+                valida = false;
+            }
+            
+            int contadorDigitos = 0;
+            int i = 0;
+            while (i < contraseña.length()) {
+                if (Character.isDigit(contraseña.charAt(i))) {
+                    contadorDigitos++;
+                }
+                i++;
+            }
+            if (contadorDigitos < 2) {
+                System.out.println("La contraseña debe tener al menos 2 dígitos.");
+                valida = false;
+            }
+            
+            if (contraseña.equalsIgnoreCase(usuario)) {
+                System.out.println("La contraseña no puede ser igual al nombre de usuario.");
+                valida = false;
+            }
+            
+        } while (!valida);
+        
+        System.out.println("Contraseña correcta.");
+        
     }
-    
 }
